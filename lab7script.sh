cf58b3992f4ffb77f5ee594c067852c1293ff3f0
Harryshield14 <fyr5ht@virginia.edu>
2025-04-22 02:02:27 +0000

    new changes

--- lab7script.sh
+++ lab7script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update -y
-apt install -y python3
-apt install -y git
-apt install -y nginx
+/usr/bin/apt update -y
+/usr/bin/apt install -y python3
+/usr/bin/apt install -y git
+/usr/bin/apt install -y nginx
